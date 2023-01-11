0d253255e0ec5ef8321a064649b20959369cb70d
Lucas de Souza Resende <99922083+LucasSResende@users.noreply.github.com>
2023-01-11 16:29:54 -0300

    Tipos

---
+++ funcoes/tipos Declaração.js
@@ -0,0 +1,17 @@
+console.log(soma(3, 4))
+
+//function declration
+function soma(x, y){
+    return x + y
+}
+
+//function expression
+const sub = function (x, y) {
+    return x - y
+}
+console.log(sub(3, 4))
+
+//named function expression
+const mult = function multi(x, y) {
+    return x * y
+}
